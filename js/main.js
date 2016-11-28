1c348692ef3a570de939198cb7345e385776b251
jj <lijiajie95@gmail.com>
2016-11-27 19:23:00 -0500

    Updated main page
    Fixed css on mobile devices, and highlighted sections nav

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-// Freelancer Theme JavaScript
-
+// Main JavaScript
 (function($) {
     "use strict"; // Start of use strict
+	
+	var header_off = $('header').height();
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
+
+    // jQuery for page scrolling feature
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
 		var name = $anchor.attr('href');
@@ -12,36 +14,26 @@
 		if (current.attr('id') != name) {
 			$('.section-div').hide("slow");
 			$('#'+name).show("fast");
-			/*$('html, body').stop(true, false).animate({
-				scrollTop: ($($anchor.attr('href')).offset().top)
-			}, 1250, 'easeInOutExpo');*/
+			$('.page-scroll').removeClass("active");
+			$anchor.parent().addClass("active");
+			/*$('html,body').animate({
+				scrollTop: header_off
+			});*/
 		}
         event.preventDefault();
     });
 
     // Highlight the top nav as scrolling occurs
     /*$('body').scrollspy({
-        target: '.navbar-top',
-        offset: 100
+        target: '#mainNav'
     });*/
 
     // Offset for Main Navigation
     $('#mainNav').affix({
         offset: {
-            top: $('header').height()
+            top: header_off
         }
     })
-
-    // Floating label headings for the contact form
-    $(function() {
-        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-        }).on("focus", ".floating-label-form-group", function() {
-            $(this).addClass("floating-label-form-group-with-focus");
-        }).on("blur", ".floating-label-form-group", function() {
-            $(this).removeClass("floating-label-form-group-with-focus");
-        });
-    });
 	
 	// Change PDF files for writing assignment
 	$('.pdf-switch').click(function() {
@@ -50,6 +42,7 @@
 		$('.main-frame').attr('data', pdf_url);
 	});
 	
+	// Scroll to contact section
 	$('#scroll-contact').click(function() {
 		$('html,body').animate({
 			scrollTop: $('.contact-footer').offset().top
